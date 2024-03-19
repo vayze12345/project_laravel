2b4d60c727cfb8c50178765303f8bf3187db1a3e
vayze12345 <vayzemounah@gmail.com>
2024-03-19 15:29:00 +0100

    commit 1

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get ('/blog/index',function(){
+
+         return 'hello word';
+
+});
+
+Route::get ('/blog/index',function(){
+    return[
+"name" => $_GET['name'],
+    "article" => "article1"
+    ];
+
+});
